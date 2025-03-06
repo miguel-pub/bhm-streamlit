e71af519fdfe7b4a6043d3cd458e5431520cc8b1
miguel-pub <miguelebersbach+1@outook.com>
2025-03-06 11:23:35 +0100

    test

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ kursliste = get_pdf_url()
 selection = st.selectbox("Wähle deine Umschulung aus:", list(get_pdf_url().keys()))
 st.write(f"{kursliste[selection]}")
 
+with open(f"KW10/{selection}-Berichtsheft-KW10.pdf", "rb") as pdf_file:
+    st.download_button(
+        label="Download Berichtsheft",
+        data=pdf_file,
+        file_name=f"{selection}-Berichtsheft-KW10.pdf",
+        mime="application/pdf"
+    )
